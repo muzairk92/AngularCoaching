11c3f73791f0451f111082238edebff4c6d1df17
AQ-kasmani <aqkasmanideveloper@gmail.com>
2017-12-09 12:12:51 +0500

    add remove functionality

--- js/controller/todo.js
+++ js/controller/todo.js
@@ -1,13 +1,24 @@
 
 app.controller('todoCtrl', function($scope,authServices,todoServices) {
-
+ $scope.allTodos;
 
     $scope.addTodo = function (todo) {
         todoServices.add(todo);
+        $scope.getAllTodo();
     };
 
     $scope.getAllTodo = function(){
+        $scope.allTodos = todoServices.getAllTodo();
     };
 
+    $scope.deleteTodo = function(data){
+        console.log(data);
+        todoServices.deleteTodo(data)
+    };
+
+    $scope.updateTodo = function(data){
+        console.log(data);
+        todoServices.updateTodo(data)
+    };
     console.log('Working Todo');
 });
--- js/service/todo.js
+++ js/service/todo.js
@@ -1,26 +1,27 @@
 
 
 app.service('todoServices', function(){
-
+    var allTodo = [];
     this.id = 0;
-    var allTodo=[];
 
     this.add = function(todo) {
         todo.id = this.id++;
-       allTodo.push(todo);
-        return console.log(allTodo);
-
+        allTodo.push(todo);
+        //return console.log(allTodo);
     };
 
-    this.deleteTodo = function(user) {
-
-
+    this.deleteTodo = function(data) {
+        var removed = allTodo.splice(data.id,1);
+        if(!removed.length>0) return console.log('something is wrong in delete');
+        return console.log('successfully delete');
     };
     this.updateTodo = function(user) {
-
+        var updateUser = {};
+        allTodo.splice(user.id,1,updateUser)
     };
 
     this.getAllTodo = function(){
+        return allTodo;
     };
 
     this.todoById = function(user) {
